4239c2590bf8250998f49e30949ee94ab8f5dd4d
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-10-31 23:31:10 +0530

    video null issue solving

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,6 +4,7 @@ import VideoCard from "./VideoCard";
 import ChannelCard from "./ChannelCard";
 
 const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading...";
   return (
     <Stack
       direction={direction || "row"}
